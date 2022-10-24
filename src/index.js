097d2b3f6c84841be980474860107c4571af2903
Erhal <odhernes@gmail.com>
2022-10-24 14:18:40 +0400

    Create ScrollToTop effect

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom";
 import AppProvider from "./providers/AppProvider";
+import ScrollToTop from "./helpers/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <AppProvider>
             <Router>
-                <App/>
+                <ScrollToTop>
+                    <App/>
+                </ScrollToTop>
             </Router>
         </AppProvider>
     </React.StrictMode>
