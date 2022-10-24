ecddc46d49b92da57c2eb0dcb8fca68f650879ee
Erhal <odhernes@gmail.com>
2022-10-24 15:16:12 +0400

    Update Cart

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,36 +1,46 @@
-import React, {useContext, useEffect, useState} from 'react';
+import {useContext, useEffect} from 'react';
+import {Link, useNavigate} from "react-router-dom";
+import {toast} from "react-toastify";
 import AppContext from "../../providers/AppContext";
+
 import CartProductCard from "../Cards/CartProductCard";
 import ProductsSection from "../ProductsSection";
-import {useNavigate} from "react-router-dom";
 
 const Cart = () => {
-    const {cartProducts, getTotalQuantity, getTotalPrice} = useContext(AppContext);
-    const [totalQuantity, setTotalQuantity] = useState(getTotalQuantity())
+    const notifyInfo = (message) => toast.info(<div className='text-center text-dark'> {message} </div>);
+    const {cartProducts, getTotalPrice} = useContext(AppContext);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (cartProducts.length === 0) navigate('/');
+        if (cartProducts.length === 0) {
+            navigate('/');
+            notifyInfo('Your cart is empty.');
+        }
     }, [cartProducts]);
 
     return (
         <>
-            <div className='Cart d-flex justify-content-center my-5'>
-                {cartProducts.length > 0 &&
-                    <>
-                        {cartProducts?.map((product) => {
-                            return <CartProductCard key={product.id} product={product}/>
-                        })}
-                    </>
-                }
+            <div className='Cart d-flex flex-column justify-content-center my-5 mx-30pc'>
+                {cartProducts.length > 0 && cartProducts?.map((product) => <CartProductCard key={product.id} product={product}/>)}
             </div>
 
-            <div className='recommended'>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <h2 className="text-center">Recommended for you</h2>
-                        </div>
+            <div className="row justify-content-between mb-5">
+                <div className="col-4"></div>
+                <div className="col-2 mb-4 d-flex align-items-center">
+                    <h5 className="fw-normal mb-0 text-black">Subtotal: ${getTotalPrice()}</h5>
+                </div>
+                <div className="col-2 mb-4 text-end">
+                    <Link to={'/cart'}>
+                        <button className="btn btn-secondary btn-block" type="button">BUY NOW</button>
+                    </Link>
+                </div>
+                <div className="col-4"></div>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h2 className="text-center">Recommended for you</h2>
                     </div>
                 </div>
             </div>
